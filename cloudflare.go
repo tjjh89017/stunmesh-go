7ebf3057ead0de9050e012a93f1f264103d1a493
Aotokitsuruya <contact@aotoki.me>
2024-08-10 21:54:09 +0800

    Refactor to split cloudflare

---
+++ cloudflare.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"context"
+	"sync"
+
+	"github.com/cloudflare/cloudflare-go"
+)
+
+type CloudflareApi interface {
+	DNSRecords(ctx context.Context, zoneId string, rr cloudflare.DNSRecord) ([]cloudflare.DNSRecord, error)
+	CreateDNSRecord(ctx context.Context, zoneId string, rr cloudflare.DNSRecord) (*cloudflare.DNSRecordResponse, error)
+	UpdateDNSRecord(ctx context.Context, zoneId, recordId string, rr cloudflare.DNSRecord) error
+	DeleteDNSRecord(ctx context.Context, zoneId, recordId string) error
+	ZoneIDByName(zoneName string) (string, error)
+}
+
+var _ Store = &CloudflareStore{}
+
+type CloudflareStore struct {
+	mutex    sync.RWMutex
+	api      CloudflareApi
+	zoneId   string
+	zoneName string
+}
+
+func NewCloudflareStore(api CloudflareApi, zoneName string) *CloudflareStore {
+	return &CloudflareStore{api: api, zoneName: zoneName}
+}
+
+func (s *CloudflareStore) Get(ctx context.Context, key string) (string, error) {
+	records, err := s.associatedRecords(ctx, key)
+	if err != nil {
+		return "", err
+	}
+
+	isFound := len(records) > 0
+	if !isFound {
+		return "", ErrEndpointDataNotFound
+	}
+
+	return records[0].Content, nil
+}
+
+func (s *CloudflareStore) Set(ctx context.Context, key string, value string) error {
+	records, err := s.associatedRecords(ctx, key)
+	if err != nil {
+		return err
+	}
+
+	zoneId, err := s.ZoneId()
+	if err != nil {
+		return err
+	}
+
+	record := cloudflare.DNSRecord{
+		Type:    "TXT",
+		Name:    key + "." + s.zoneName,
+		Content: value,
+	}
+
+	isFound := len(records) > 0
+	if !isFound {
+		_, err := s.api.CreateDNSRecord(ctx, zoneId, record)
+		return err
+	}
+
+	isDuplicate := len(records) > 1
+	if isDuplicate {
+		for _, x := range records[1:] {
+			if err := s.api.DeleteDNSRecord(ctx, zoneId, x.ID); err != nil {
+				continue
+			}
+		}
+	}
+
+	return s.api.UpdateDNSRecord(ctx, zoneId, records[0].ID, record)
+}
+
+func (s *CloudflareStore) ZoneId() (string, error) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if s.zoneId == "" {
+		zone, err := s.api.ZoneIDByName(s.zoneName)
+		if err != nil {
+			return "", err
+		}
+
+		s.zoneId = zone
+	}
+
+	return s.zoneId, nil
+}
+
+func (s *CloudflareStore) associatedRecords(ctx context.Context, key string) ([]cloudflare.DNSRecord, error) {
+	zoneId, err := s.ZoneId()
+	if err != nil {
+		return nil, err
+	}
+
+	return s.api.DNSRecords(ctx, zoneId, cloudflare.DNSRecord{Type: "TXT", Name: key + "." + s.zoneName})
+}
---
+++ cloudflare_test.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"testing"
+
+	"github.com/cloudflare/cloudflare-go"
+)
+
+var _ CloudflareApi = &mockCloudflareApi{}
+
+type mockCloudflareApi struct {
+	mutex   sync.RWMutex
+	lastId  int
+	records []cloudflare.DNSRecord
+}
+
+func newMockCloudflareApi() *mockCloudflareApi {
+	return &mockCloudflareApi{
+		records: []cloudflare.DNSRecord{},
+	}
+}
+
+func (m *mockCloudflareApi) DNSRecords(ctx context.Context, zoneId string, rr cloudflare.DNSRecord) ([]cloudflare.DNSRecord, error) {
+	m.mutex.RLock()
+	defer m.mutex.RUnlock()
+
+	matchedRecords := []cloudflare.DNSRecord{}
+
+	for _, record := range m.records {
+		isNameMatched := record.Name == rr.Name
+		isTypeMatched := record.Type == rr.Type
+
+		if isNameMatched && isTypeMatched {
+			matchedRecords = append(matchedRecords, record)
+		}
+	}
+
+	return matchedRecords, nil
+}
+
+func (m *mockCloudflareApi) CreateDNSRecord(ctx context.Context, zoneId string, rr cloudflare.DNSRecord) (*cloudflare.DNSRecordResponse, error) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
+	m.lastId++
+	rr.ID = fmt.Sprintf("mock-record-id-%d", m.lastId)
+
+	m.records = append(m.records, rr)
+	return &cloudflare.DNSRecordResponse{}, nil
+}
+
+func (m *mockCloudflareApi) UpdateDNSRecord(ctx context.Context, zoneId, recordId string, rr cloudflare.DNSRecord) error {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
+	for i, record := range m.records {
+		if record.ID == recordId {
+			m.records[i].Content = rr.Content
+			return nil
+		}
+	}
+
+	return fmt.Errorf("record with id %s not found", recordId)
+}
+
+func (m *mockCloudflareApi) DeleteDNSRecord(ctx context.Context, zoneId, recordId string) error {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
+	for i, record := range m.records {
+		if record.ID == recordId {
+			m.records = append(m.records[:i], m.records[i+1:]...)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("record with id %s not found", recordId)
+}
+
+func (m *mockCloudflareApi) ZoneIDByName(zoneName string) (string, error) {
+	return "mock-zone-id", nil
+}
+
+func Test_CloudflareStore(t *testing.T) {
+	t.Parallel()
+
+	mockApi := newMockCloudflareApi()
+	store := NewCloudflareStore(mockApi, "example.com")
+	ctx := context.Background()
+
+	key := "key"
+	value := "value"
+
+	err := store.Set(ctx, key, value)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	gotValue, err := store.Get(ctx, key)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if gotValue != value {
+		t.Fatalf("expected value %s, got %s", value, gotValue)
+	}
+}
+
+func Test_CloudflareStore_ExistsDuplicate(t *testing.T) {
+	mockApi := newMockCloudflareApi()
+	store := NewCloudflareStore(mockApi, "example.com")
+	ctx := context.Background()
+
+	for i := 0; i < 3; i++ {
+		_, err := mockApi.CreateDNSRecord(ctx, "mock-zone-id", cloudflare.DNSRecord{
+			Type:    "TXT",
+			Content: fmt.Sprintf("value-%d", i),
+			Name:    "key.example.com",
+		})
+
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	key := "key"
+	value := "value"
+
+	err := store.Set(ctx, key, value)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	gotValue, err := store.Get(ctx, key)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if gotValue != value {
+		t.Fatalf("expected value %s, got %s", value, gotValue)
+	}
+}
