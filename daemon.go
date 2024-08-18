7c693728d2d41c20bcc3bd7236e023ea4f4f85ea
Aotokitsuruya <contact@aotoki.me>
2024-08-18 21:05:23 +0800

    Add daemonize support

---
+++ daemon.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+const RefreshInterval = time.Duration(10) * time.Second
+
+func Run(ctx context.Context, privateKey [32]byte, ctrl *Controller, peers []*Peer) {
+	daemonCtx, cancel := context.WithCancel(ctx)
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	defer func() {
+		log.Println("Shutting down")
+		signal.Stop(signalChan)
+		close(signalChan)
+		cancel()
+	}()
+
+	for {
+		select {
+		case <-daemonCtx.Done():
+			return
+		case <-signalChan:
+			return
+		case <-time.Tick(RefreshInterval):
+			log.Println("Refreshing peers")
+
+			for _, peer := range peers {
+				serializer := NewCryptoSerializer(privateKey, peer.PublicKey())
+
+				ctrl.Publish(daemonCtx, serializer, peer)
+				ctrl.Establish(daemonCtx, serializer, peer)
+			}
+		}
+	}
+}
