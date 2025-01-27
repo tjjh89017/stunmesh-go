e62eb64633c4a172c1a554a712ca954a8114cdc9
Date Huang <tjjh89017@hotmail.com>
2025-01-27 23:30:31 +0800

    darwin: stun: support macos with wireguard-go version
    We don't support Wireguard App version currently.
    Don know how to do it.
    
    Signed-off-by: Date Huang <tjjh89017@hotmail.com>

--- go.mod
+++ go.mod
@@ -1,10 +1,13 @@
 module github.com/tjjh89017/stunmesh-go
 
-go 1.22
+go 1.23.0
+
+toolchain go1.23.1
 
 require (
 	github.com/cloudflare/cloudflare-go v0.114.0
 	github.com/google/wire v0.6.0
+	github.com/packetcap/go-pcap v0.0.0-20240528124601-8c87ecf5dbc5
 	github.com/pion/stun v0.6.1
 	github.com/rs/zerolog v1.33.0
 	github.com/spf13/viper v1.19.0
@@ -19,6 +22,7 @@ require (
 	github.com/goccy/go-json v0.10.4 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/gopacket/gopacket v1.2.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/josharian/native v1.1.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
@@ -34,6 +38,7 @@ require (
 	github.com/pion/transport/v2 v2.2.1 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
@@ -50,3 +55,5 @@ require (
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
+
+replace github.com/packetcap/go-pcap v0.0.0-20240528124601-8c87ecf5dbc5 => github.com/tjjh89017/go-pcap v0.0.0-20250128104425-0bc5e44f83eb
--- internal/stun/stun_darwin.go
+++ internal/stun/stun_darwin.go
@@ -1,23 +1,226 @@
 // build +darwin
 package stun
 
-import "context"
+import (
+	"context"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"net"
+	"sync"
+	"syscall"
+	"time"
 
-// NOTE: This is a placeholder implementation for darwin.
+	pcap "github.com/packetcap/go-pcap"
+	"github.com/pion/stun"
+	"github.com/rs/zerolog"
+	"golang.org/x/net/ipv4"
+	"golang.org/x/net/route"
+)
+
+const PacketSize = 1600
+
+const (
+	etherOff   = 0
+	ipOff      = etherOff + 14
+	udpOff     = ipOff + 5*4
+	payloadOff = udpOff + 2*4
+)
+
 type Stun struct {
+	port       uint16
+	conn       *ipv4.PacketConn
+	handle     *pcap.Handle
+	once       sync.Once
+	packetChan chan *stun.Message
 }
 
 func New(ctx context.Context, port uint16) (*Stun, error) {
-	return &Stun{}, nil
+
+	// use default route to be the interface to listen stun
+	defaultRouteInterface, err := getDefaultRouteInterface()
+	if err != nil {
+		return nil, err
+	}
+
+	handle, err := pcap.OpenLive(defaultRouteInterface, PacketSize, false, 0, pcap.DefaultSyscalls)
+	if err != nil {
+		return nil, err
+	}
+
+	filter := fmt.Sprintf("udp dst port %d and udp[12:4] == 0x2112A442", port)
+	if err := handle.SetBPFFilter(filter); err != nil {
+		return nil, err
+	}
+
+	c, err := net.ListenPacket("ip4:17", "0.0.0.0")
+	if err != nil {
+		return nil, err
+	}
+
+	conn := ipv4.NewPacketConn(c)
+
+	return &Stun{
+		port:       port,
+		conn:       conn,
+		handle:     handle,
+		packetChan: make(chan *stun.Message),
+	}, nil
 }
 
 func (s *Stun) Stop() error {
-	return nil
+	close(s.packetChan)
+	s.handle.Close()
+	return s.conn.Close()
 }
 
 func (s *Stun) Start(ctx context.Context) {
+	logger := zerolog.Ctx(ctx)
+
+	logger.Info().Msgf("starting to listen stun response")
+	s.once.Do(func() {
+		go func() {
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case <-time.After(time.Duration(StunTimeout) * time.Second):
+					return
+				default:
+					var (
+						buf []byte
+						err error
+					)
+					buf, _, err = s.handle.ReadPacketData()
+					if err != nil {
+						logger.Debug().Msgf("fail to read packet data")
+						continue
+					}
+					// decode STUN
+					m := &stun.Message{
+						Raw: buf[payloadOff:],
+					}
+					if err := m.Decode(); err != nil {
+						logger.Debug().Msgf("fail to decode stun msg")
+						continue
+					}
+					s.packetChan <- m
+					return
+				}
+			}
+		}()
+	})
 }
 
-func (s *Stun) Connect(ctx context.Context, address string) (_ string, _ int, err error) {
-	return "", 0, nil
+func (s *Stun) Connect(ctx context.Context, stunAddr string) (_ string, _ int, err error) {
+	logger := zerolog.Ctx(ctx)
+
+	logger.Info().Msgf("connecting to STUN server: %s", stunAddr)
+	addr, err := net.ResolveUDPAddr("udp4", stunAddr)
+	if err != nil {
+		return "", 0, err
+	}
+
+	packet, err := createStunBindingPacket(s.port, uint16(addr.Port))
+	if err != nil {
+		return "", 0, err
+	}
+
+	_, err = s.conn.WriteTo(packet, nil, addr)
+	if err != nil {
+		return "", 0, err
+	}
+
+	reply, err := s.Read(ctx)
+	if err != nil {
+		return "", 0, err
+	}
+
+	replyAddr := Parse(ctx, reply)
+
+	return replyAddr.IP.String(), replyAddr.Port, nil
+}
+
+func (s *Stun) Read(ctx context.Context) (*stun.Message, error) {
+	select {
+	case m := <-s.packetChan:
+		return m, nil
+	case <-time.After(time.Duration(StunTimeout) * time.Second):
+		return nil, ErrTimeout
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	}
+}
+
+func createStunBindingPacket(srcPort, dstPort uint16) ([]byte, error) {
+	msg, err := stun.Build(stun.TransactionID, stun.BindingRequest)
+	if err != nil {
+		return nil, err
+	}
+	_ = msg.NewTransactionID()
+
+	packetLength := uint16(BindingPacketHeaderSize + len(msg.Raw))
+	checksum := uint16(0)
+
+	buf := make([]byte, BindingPacketHeaderSize)
+	binary.BigEndian.PutUint16(buf[0:], srcPort)
+	binary.BigEndian.PutUint16(buf[2:], dstPort)
+	binary.BigEndian.PutUint16(buf[4:], packetLength)
+	binary.BigEndian.PutUint16(buf[6:], checksum)
+
+	return append(buf, msg.Raw...), nil
+}
+
+func getDefaultRouteInterface() (string, error) {
+	// Fetch the routing information base (RIB) for routes.
+	rib, err := route.FetchRIB(syscall.AF_INET, route.RIBTypeRoute, 0)
+	if err != nil {
+		fmt.Println("Error fetching RIB:", err)
+		return "", err
+	}
+
+	msgs, err := route.ParseRIB(route.RIBTypeRoute, rib)
+	if err != nil {
+		fmt.Println("Error parsing RIB:", err)
+		return "", err
+	}
+
+	var dstIP string
+	var netmask string
+	var ifaceName string
+
+	for _, msg := range msgs {
+		switch m := msg.(type) {
+		case *route.RouteMessage:
+			// Extract destination address.
+			if len(m.Addrs) > syscall.RTAX_NETMASK {
+				switch addr := m.Addrs[syscall.RTAX_DST].(type) {
+				case *route.Inet4Addr:
+					dstIP = net.IPv4(addr.IP[0], addr.IP[1], addr.IP[2], addr.IP[3]).String()
+				}
+				switch addr := m.Addrs[syscall.RTAX_NETMASK].(type) {
+				case *route.Inet4Addr:
+					netmask = net.IPv4Mask(addr.IP[0], addr.IP[1], addr.IP[2], addr.IP[3]).String()
+				}
+			}
+
+			// Extract interface index and name using the Index field.
+			ifaceIndex := m.Index
+			if ifaceIndex != 0 {
+				iface, err := net.InterfaceByIndex(ifaceIndex)
+				if err == nil {
+					ifaceName = iface.Name
+				} else {
+					ifaceName = fmt.Sprintf("if%d", ifaceIndex)
+				}
+			}
+
+			if dstIP == "0.0.0.0" && netmask == "00000000" {
+				fmt.Printf("%v %v %v\n", dstIP, netmask, ifaceName)
+				return ifaceName, nil
+			}
+		}
+	}
+
+	return "", errors.New("No default route found")
 }
