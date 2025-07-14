655ac565e4087b86dc0c6bf35aed51a95288e645
Date Huang <tjjh89017@hotmail.com>
2025-07-14 15:36:21 +0800

    f
    Signed-off-by: Date Huang <tjjh89017@hotmail.com>

--- internal/ctrl/establish_freebsd.go
+++ internal/ctrl/establish_freebsd.go
@@ -6,16 +6,18 @@ import (
 	"context"
 	"encoding/base64"
 	"os/exec"
+	"strconv"
 
 	"github.com/tjjh89017/stunmesh-go/internal/entity"
 )
 
 func (c *EstablishController) ConfigureDevice(ctx context.Context, peer *entity.Peer, res *EndpointDecryptResponse) error {
-	c.logger.Debug().Str("peer", peer.LocalId()).Str("remote", res.Host+":"+string(res.Port)).Msg("configuring device for peer")
+	remoteEndpoint := res.Host + ":" + strconv.FormatInt(int64(res.Port), 10)
+	c.logger.Debug().Str("peer", peer.LocalId()).Str("remote", remoteEndpoint).Msg("configuring device for peer")
 
 	var publicKeyArray [32]byte = peer.PublicKey()
 	publicKey := base64.StdEncoding.EncodeToString(publicKeyArray[:])
-	r, err := exec.Command("wg", "set", peer.DeviceName(), "peer", publicKey, "endpoint", res.Host+":"+string(res.Port)).Output()
+	r, err := exec.Command("wg", "set", peer.DeviceName(), "peer", publicKey, "endpoint", remoteEndpoint).Output()
 	c.logger.Debug().Str("output", string(r)).Msg("wg set command executed")
 	return err
 }
