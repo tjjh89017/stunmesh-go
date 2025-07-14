614262839b9fc28225bdd026d4a037fee90f9c3e
Date Huang <tjjh89017@hotmail.com>
2025-07-14 15:52:06 +0800

    f
    Signed-off-by: Date Huang <tjjh89017@hotmail.com>

--- internal/ctrl/establish_freebsd.go
+++ internal/ctrl/establish_freebsd.go
@@ -17,6 +17,24 @@ func (c *EstablishController) ConfigureDevice(ctx context.Context, peer *entity.
 
 	var publicKeyArray [32]byte = peer.PublicKey()
 	publicKey := base64.StdEncoding.EncodeToString(publicKeyArray[:])
+
+	// compare the current endpoint with the existing one
+	device, err := c.wgCtrl.Device(peer.DeviceName())
+	if err != nil {
+		c.logger.Error().Err(err).Msg("failed to get device")
+		return err
+	}
+	if device != nil {
+		for _, p := range device.Peers {
+			if p.PublicKey.String() == publicKey {
+				if p.Endpoint.String() == remoteEndpoint {
+					c.logger.Debug().Msg("endpoint already configured, skipping")
+					return nil
+				}
+			}
+		}
+	}
+
 	r, err := exec.Command("wg", "set", peer.DeviceName(), "peer", publicKey, "endpoint", remoteEndpoint).Output()
 	c.logger.Debug().Str("output", string(r)).Msg("wg set command executed")
 	return err
