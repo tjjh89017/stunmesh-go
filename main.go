11debda020d3241c3d121293ed9649a9aeda29d5
Date Huang <tjjh89017@hotmail.com>
2025-07-17 15:25:51 +0800

    args: oneshot command for 3 time for all peers
    Signed-off-by: Date Huang <tjjh89017@hotmail.com>

--- main.go
+++ main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"context"
+	"flag"
+	"os"
 )
 
 func main() {
 	ctx := context.Background()
+
+	oneshot := flag.Bool("oneshot", false, "run in oneshot mode (publish and establish 3 times, then exit)")
+	flag.Parse()
 
 	daemon, err := setup()
 	if err != nil {
 		panic(err)
 	}
 
-	daemon.Run(ctx)
+	if *oneshot {
+		daemon.RunOneshot(ctx)
+		os.Exit(0)
+	} else {
+		daemon.Run(ctx)
+	}
 }
