abdd1179203e7209f00cdf05a67ab5977c647d8d
Date Huang <tjjh89017@hotmail.com>
2025-07-20 17:00:27 +0800

    f
    Signed-off-by: Date Huang <tjjh89017@hotmail.com>

--- internal/stun/resolver.go
+++ internal/stun/resolver.go
@@ -24,7 +24,7 @@ func NewResolver(config *config.Config, logger *zerolog.Logger) *Resolver {
 func (r *Resolver) Resolve(ctx context.Context, deviceName string, port uint16) (_ string, _ int, err error) {
 	r.mu.Lock()
 	defer r.mu.Unlock()
-	
+
 	stunCtx := r.logger.WithContext(ctx)
 
 	stun, err := New(stunCtx, deviceName, port)
@@ -35,6 +35,7 @@ func (r *Resolver) Resolve(ctx context.Context, deviceName string, port uint16) 
 	stun.Start(stunCtx)
 	defer func() {
 		err = stun.Stop()
+		r.logger.Trace().Msg("resolve stopped")
 	}()
 
 	return stun.Connect(stunCtx, r.config.Stun.Address)
